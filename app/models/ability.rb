34a6d1aa3506b450b13c29c46ef88ea7558a841d
kkolpa <kkolpa04@gmail.com>
2018-09-26 13:32:45 +0200

    5.10 user ability fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    if user.admin?
+  #  user ||= User.new # guest user (not logged in)
+    if user.nil?
+      user = User.new
+    elsif user.admin?
       can :manage, :all
     else
       can :manage, User, id: user.id
-      can :manage, Comment, id: user.id
-      cannot :destroy, Comment
+
+      can :manage, Comment.where(user_id: user.id) do |comment|
+    end
+
+    cannot [:destroy, :delete], Comment.where(user_id: user.id) do |comment|
+      comment.user_id == user.id
+    end
+  end
   end
 end
